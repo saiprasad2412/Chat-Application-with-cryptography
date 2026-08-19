863ae0d70197a64e82cd6ea4aa562d7894d4bf05
saiprasad2412 <manesaiprasad2412@gmail.com>
2026-08-19 08:38:28 +0530

    middleware creations

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,17 @@
 import express from "express"
 import "dotenv/config"
 import { connectDB } from "./lib/db.js";
+import {clerkMiddleware} from '@clerk/express';
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+//middlewares
+app.use(express.json());
+app.use(cors({origin:CLIENT_URL, credentials:true})); //to enable diff port data access
+app.use(clerkMiddleware());
 
 app.get("/h",(req, res)=>{
     res.status(200).json({
